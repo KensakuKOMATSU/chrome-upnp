cb2bd8206a74070bae50d13ff8fd75674f2bea16
Kensaku KOMATSU <kensaku.komatsu@gmail.com>
2012-07-25 14:24:05 +0900

    my 1st commit

---
+++ javascripts/upnp.js
@@ -0,0 +1,108 @@
+var UPnP;
+
+(function(){ 
+  /**
+   * utilities
+   *
+   */
+
+  // translate text string to Arrayed buffer
+  //
+  function t2ab(str /* String */) {
+    var buffer = new ArrayBuffer(str.length);
+    var view = new DataView(buffer);
+    for(var i = 0, l = str.length; i < l; i++) {
+      view.setInt8(i, str.charAt(i).charCodeAt());
+    }
+    return buffer;
+  }
+
+  // translate Arrayed buffer to text string
+  //
+  function ab2t(buffer /* ArrayBuffer */) {
+    var arr = new Int8Array(buffer);
+    var str = "";
+    for(var i = 0, l = arr.length; i < l; i++) {
+      str += String.fromCharCode.call(this, arr[i]);
+    }
+    console.log(str);
+    return str;
+  }
+
+
+  /**
+   * You can find API documentaion for raw socket api in 
+   * http://code.google.com/chrome/extensions/trunk/experimental.socket.html
+   *
+   */
+
+  // SSDP definitions
+  var M_SEARCH_REQUEST = "M-SEARCH * HTTP/1.1\r\n" +
+    "MX: 3\r\n" +
+    "HOST: 239.255.255.250:1900\r\n" +
+    "MAN: \"ssdp:discover\"\r\n" +
+    "ST: upnp:rootdevice\r\n\r\n"
+
+  // UPnP classes
+  UPnP = function(){
+    this.sid = null;
+    this.MIP_ = "239.255.255.250";
+    this.PORT_ = 1900;
+
+    var self = this;
+    chrome.experimental.socket.create('udp', {}, function(socketInfo) {
+      self.sid = socketInfo.socketId;
+      chrome.experimental.socket.bind(self.sid, "0.0.0.0", 0, function(res) {
+        if(res !== 0) {
+          throw('cannot bind socket');
+        }
+        self.onready();
+      });
+    });
+  }
+
+  // interface to onready
+  UPnP.prototype.onready = function() {
+  }
+
+  // do M-SEARCH
+  UPnP.prototype.search = function(callback /* function */) {
+    if(!!this.sid === false) {
+      throw('socket id is not allocated');
+    }
+
+    var buffer = t2ab(M_SEARCH_REQUEST);
+
+    // send M-SEARCH twice times
+    for(var i = 0; i < 2; i++) {
+      chrome.experimental.socket.sendTo(this.sid, buffer, this.MIP_, this.PORT_, function(e) {
+        if(e.bytesWritten < 0) {
+          throw("an Error occured while sending M-SEARCH : "+e.bytesWritten);
+        }
+        console.log("=== SENT UPnP M-SEARCH ===");
+        console.log(e);
+
+        if(typeof(callback) === "function")
+          callback();
+      });
+    }
+  }
+
+  // listen response to M-SEARCH
+  UPnP.prototype.listen = function(callback) {
+    if(!!this.sid === false) {
+      throw('socket id is not allocated');
+    }
+
+    var self = this;
+    chrome.experimental.socket.recvFrom(this.sid, null, function(recv) {
+      console.log("=== RECV UPnP packet from "+recv.address+"===");
+      console.log(recv);
+      recv.data = ab2t(recv.data);
+      if(typeof(callback) === "function") {
+        callback(recv);
+      }
+      self.listen(callback);
+    });
+  }
+}());
