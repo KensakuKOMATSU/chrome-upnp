0bf3db96f19e5889cd5a2047bc2f7f92671eeedb
小松 健作 <kensaku.komatsu@gmail.com>
2012-07-27 11:42:46 +0900

    create crx

--- testserver/app.js
+++ testserver/app.js
@@ -31,6 +31,8 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
