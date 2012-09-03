4f79a71a88c4d29cd788ce3062a981370ab2877c
kensakukomatsu <kensaku.komatsu@gmail.com>
2012-09-03 17:38:09 +0900

    change setAVTransport xml format

--- apps/javascripts/airplay_.js
+++ apps/javascripts/airplay_.js
@@ -1,7 +1,6 @@
+var videourl = (launchData && launchData.intent && launchData.intent.data) || "http://192.168.0.9:3000/vmenu/videos/bunny.mp4";
 
-window.intent = window.intent || window.webkitIntent;
-
-var videourl = ( window.intent && window.intent.data ) || "";
+console.log(launchData && launchData.intent);
 
 $("#videourl").val(videourl).bind("input", function(e) {
   videourl = $(this).val();
@@ -30,17 +29,18 @@ function check_overlapped(o, list){
 var show = function(recv){
   var str = recv.data;
   var o = parse(str);
+  console.log(o);
   var origin = o.location.split("/").slice(0,3).join("/")+ "/";
 
   if(check_overlapped(o,list)) return;
 
   list.push(o);
 
-  if(TARGET === ROOTDEVICE) {
+//  if(TARGET === ROOTDEVICE) {
     $("#device_lists").append("<pre>"+str+"</pre>");
-    return;
-  }
-
+//    return;
+//  }
+  console.log(list);
 
   $.get(o.location, function(data){
     console.log(data)
@@ -54,7 +54,7 @@ var show = function(recv){
     services.each(function(e){
       var service = $(this)
 
-      if(service.find("serviceType").text() == TARGET){
+      if(service.find("serviceType").text() == target){
         var controlurl = origin + service.find("controlURL").text()
 
         $("#device_lists").append("<dl><dt><img src='"+iconurl+"'></dt>")
@@ -80,7 +80,7 @@ function set_uri(control_url){
  // var object_url = "http://172.16.0.139/testmovie.mp4"
 
  // var object_url = "http://video30th.komasshu.info/videos/bunny.mp4"
-
+/*
   var data = '<?xml version="1.0" encoding="utf-8"?>'
     +'<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'
     + '<s:Body>'
@@ -89,24 +89,34 @@ function set_uri(control_url){
     + '<CurrentURI>'
     + videourl
     + '</CurrentURI>'
-    + '<CurrentURIMetaData>test</CurrentURIMetaData>'
+    + '<CurrentURIMetaData/>'
     + '</u:SetAVTransportURI>'
     + '</s:Body>'
     + '</s:Envelope>'
+    */
+    // videourl = "http://192.168.0.11:10243/WMPNSSv4/3929665367/0_e0FFQUNFRThFLUY4REItNEEzOS1CREVBLURDRjgwOTUyMTNCMn0uMC44.wmv";
+    var data = "<?xml version=\"1.0\"?>";
+      data += "\n\n";
+
+      data += "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><SOAP-ENV:Body><m:SetAVTransportURI xmlns:m=\"urn:schemas-upnp-org:service:AVTransport:1\"><InstanceID xmlns:dt=\"urn:schemas-microsoft-com:datatypes\" dt:dt=\"ui4\">0</InstanceID><CurrentURI xmlns:dt=\"urn:schemas-microsoft-com:datatypes\" dt:dt=\"string\">"+videourl+"</CurrentURI><CurrentURIMetaData xmlns:dt=\"urn:schemas-microsoft-com:datatypes\" dt:dt=\"string\"></CurrentURIMetaData></m:SetAVTransportURI></SOAP-ENV:Body></SOAP-ENV:Envelope>"
 
 
   $.ajax({
     type: "POST",
     url: control_url,
     headers: {
-      SOAPACTION: "urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI"
+      SOAPACTION: "\"urn:schemas-upnp-org:service:AVTransport:1#SetAVTransportURI\""
     },
     contentType: 'text/xml ; charset="utf-8"',
     data: data,
     success: function(msg){
       console.log(msg)
       play_uri(control_url)
-    } 
+    },
+    error: function(e) {
+      console.log(e);
+      play_uri(control_url)
+    }
   })
 }
 
@@ -129,7 +139,7 @@ function play_uri(control_url){
     type: "POST",
     url: control_url,
     headers: {
-      SOAPACTION: "urn:schemas-upnp-org:service:AVTransport:1#Play"
+      SOAPACTION: "\"urn:schemas-upnp-org:service:AVTransport:1#Play\""
     },
     contentType: 'text/xml ; charset="utf-8"',
     data: data,
@@ -139,12 +149,13 @@ function play_uri(control_url){
   })
 }
 
-
+var D;
 
 
 
 var parse = function(data) {
-  var arr = data.split("\n"), ret = {};
+  D = data;
+  var arr = data.replace(/\r\n|\r/g, "\n").split("\n"), ret = {};
 
   for(var i = 0, l = arr.length; i < l; i++ ) {
     var a = arr[i].split(":");
@@ -158,10 +169,14 @@ var parse = function(data) {
   return ret;
 }
 
-
+var target;
 // Main
-var upnp = new UPnP();
-upnp.onready = function(){
-  this.listen(show);
-  this.search(TARGET);
-}
+$("#startsearch").bind("click", function(){
+  $("#device_lists").html("start M-SEARCH...");
+  var upnp = new UPnP();
+  upnp.onready = function(){
+    target = $("#root").attr("checked") === "checked" ? ROOTDEVICE : AVTRANSPORT;
+    this.listen(show);
+    this.search(target);
+  }
+})
--- apps/main.js
+++ apps/main.js
@@ -1,6 +1,11 @@
-chrome.app.runtime.onLaunched.addListener(function(){
+var appWindow;
+
+chrome.app.runtime.onLaunched.addListener(function(launchData){
 	chrome.app.window.create('main.html', {
 		width: 680,
 		height: 480
+	}, function(w){
+		appWindow = w;
+		appWindow.launchData = launchData;
 	});
 });
